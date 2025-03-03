224ce85ebfb26dd36678ab4c859904fc23dc3722
RobAstur <157633879+RobAstur@users.noreply.github.com>
2025-03-03 18:21:36 +0000

    cards game

--- Assignment/assignment2-carddraw.py
+++ Assignment/assignment2-carddraw.py
@@ -1,7 +1,24 @@
 import requests
-from xml.dom import minidom
 
-# Shuffle the deck and get deck_id (XML response)
-response = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-doc = minidom.parseString(response.text)
-print(doc.toprettyxml(), end = "")
+# Shuffle the deck
+shuffle_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+response = requests.get(shuffle_url)
+
+# if the request is succesful, proceed with the 5 cards
+if response.status_code == 200:
+    deck_id = response.json()["deck_id"]
+
+    # Draw 5 cards from the shuffled deck
+    draw_url = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5"
+    response = requests.get(draw_url)
+
+    if response.status_code == 200:
+        cards = response.json()["cards"]
+        
+        # Loop the cards. print card value and suit 
+        for card in cards:
+            print(f"Card: {card['value']} of {card['suit']}")
+    else:
+        print("Error drawing cards.")
+else:
+    print("Error shuffling deck.")
