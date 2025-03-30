cb4f08d4909d35a8aac147efd36b90bc6a21fcbb
Roberto Gomez Garcia <gomezgar.roberto@gmail.com>
2025-03-30 17:38:28 +0100

    adding comments

--- Assignment/assignment03-cso.py
+++ Assignment/assignment03-cso.py
@@ -1,11 +1,17 @@
+
+#Import the modules
 import requests
 import json
 
+#Url for the API
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 
+# Function to get all data from the API
 def getALL():
     response = requests.get(url)
     return response.json()
+
+#open file and convert data into jason
 if __name__ == "__main__":
     with open ("cso.json", "wt") as fp:
         print(json.dumps(getALL()), file = fp)
